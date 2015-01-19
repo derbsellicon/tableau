ec826e11086193874e2710190101eb8cc0134a3a
Zakaria ElQotbi <zakaria@derbsellicon.com>
2015-01-19 23:42:37 +0100

    handle None

--- tableau.py
+++ tableau.py
@@ -9,17 +9,21 @@ def get_bus_times(bus_line, bus_station):
 	page = requests.get(url)
 	soup = BeautifulSoup(page.text,"lxml")
 	timetable = soup.find("tbody")
-	times = [ td.string for td in timetable.findAll("td")]
+	if timetable is None:
+		return None
+	times = [td.string for td in timetable.findAll("td")]
 	del times[0]
 	del times[1]
 	return times
 
 def print_bus(line):
 	times = get_bus_times(line, stations[line])
-	print 'bus:' , line
-	print 'prochain: ' + times[0]
-	print 'suivant: ' + times[1]
+	if times is None or times == [None, None]:
+		print 'Bus ' + line + ' Service Termine'
+	elif times[1] is None:
+		print 'Bus ' + line + ' Prochain: ' + times[0]
+	else:
+		print 'Bus ' + line + ' Prochain: ' + times[0] + ' Suivant: ' + times[1]
 
 print_bus('58')
-print '===================='
 print_bus('62')
