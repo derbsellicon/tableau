757158abe927b111ba5bbf75bcc3eca256069e13
Zakaria ElQotbi <zakaria@derbsellicon.com>
2015-01-20 04:02:26 +0100

    add velib

--- script.sh
+++ script.sh
@@ -31,7 +31,16 @@ do
 	pnmcat -lr images/blank16x32.ppm images/bus.ppm images/${line}.ppm tmp/msg-bus${line}.ppm images/blank16x32.ppm > tmp/bus-${line}.ppm
 done
 
-for file in tmp/ratp-bus*
+(python ./velib.py) | while read text
+do
+	station=$(echo $text | awk '{print $2}' )
+	text=$(echo $text | sed 's/ velib disponibles//')
+	ppmmake black $(echo "8.5 * $(echo $text | wc -m )"|bc) 16 > tmp/template.ppm
+	ppmlabel -x 2 -y 12 -size 10 -text "$text" tmp/template.ppm > tmp/msg-velib.ppm
+	pnmcat -lr images/blank16x32.ppm images/velib2.ppm tmp/msg-velib.ppm images/velib-logo3.ppm images/blank16x32.ppm images/blank16x32.ppm > tmp/velib-${station}.ppm
+done
+
+for file in tmp/bus-* tmp/velib-*.ppm
 do
 	sudo ./led-matrix -r 16 -t $(echo "$(head -2 $file | tail -1 | awk '{print $1}') / 25" | bc) -D 1 $file
 	#display $file
